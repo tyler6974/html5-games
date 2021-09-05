4613157a401c77264b7346d0242688ca4b71864e
Linh <5692900+yell0wsuit@users.noreply.github.com>
2021-09-05 22:59:14 +0700

    Update game.js

--- games/chrome-dino/game.js
+++ games/chrome-dino/game.js
@@ -1,7 +1,6 @@
 // Copyright (c) 2014 The Chromium Authors. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
-// extract from chromium source code by @liuwayong
 (function () {
     'use strict';
     /**

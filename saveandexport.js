70b392d6dad17f1b973e39d57e2e6cca65d3487b
Linh <5692900+yell0wsuit@users.noreply.github.com>
2021-09-22 21:58:15 +0700

    Add modal dialogs

--- saveandexport.js
+++ saveandexport.js
@@ -1,42 +1,49 @@
-    const saveTemplateAsFile = (filename, dataObjToWrite) => {
-    const blob = new Blob([JSON.stringify(dataObjToWrite)], { type: "text/json" });
-    const link = document.createElement("a");
+const saveTemplateAsFile = (filename, dataObjToWrite) => {
+        const blob = new Blob([JSON.stringify(dataObjToWrite)], {
+            type: "text/json"
+        });
+        const link = document.createElement("a");
 
-    link.download = filename;
-    link.href = window.URL.createObjectURL(blob);
-    link.dataset.downloadurl = ["text/json", link.download, link.href].join(":");
+        link.download = filename;
+        link.href = window.URL.createObjectURL(blob);
+        link.dataset.downloadurl = ["text/json", link.download, link.href].join(":");
 
-    const evt = new MouseEvent("click", {
-        view: window,
-        bubbles: true,
-        cancelable: true,
-    });
+        const evt = new MouseEvent("click", {
+            view: window,
+            bubbles: true,
+            cancelable: true,
+        });
 
-    link.dispatchEvent(evt);
-    link.remove()
+        link.dispatchEvent(evt);
+        link.remove();
+
 };
-const items = { ...localStorage };
+const items = {
+    ...localStorage
+};
 
 var input = document.createElement('input');
 input.type = 'file';
 input.accept = 'application/json';
 
-input.onchange = function(e) { 
-var ext = this.value.match(/\.([^.]+)$/)[1];
-switch (ext) {case 'json':
-   var file = e.target.files[0]; 
-   var reader = new FileReader();
-   reader.readAsText(file,'UTF-8');
-   reader.onload = readerEvent => {
-      var content = readerEvent.target.result;
-      var data = JSON.parse(content);
-Object.keys(data).forEach(function (k) {
-  localStorage.setItem(k, data[k]);
-});
-alert('Successfully imported game data.');
-   }
-break;
-default:
-alert('Invalid file type.');
-this.value='';}
+input.onchange = function(e) {
+    var ext = this.value.match(/\.([^.]+)$/)[1];
+    switch (ext) {
+        case 'json':
+            var file = e.target.files[0];
+            var reader = new FileReader();
+            reader.readAsText(file, 'UTF-8');
+            reader.onload = readerEvent => {
+                var content = readerEvent.target.result;
+                var data = JSON.parse(content);
+                Object.keys(data).forEach(function(k) {
+                    localStorage.setItem(k, data[k]);
+                });
+                $('#importModal').modal('show');
+            }
+            break;
+        default:
+            $('#importFailedModal').modal('show');
+            this.value = '';
+    }
 }

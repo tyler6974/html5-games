ee409f52c5539ff718e4757a3466463d1de22660
Linh <5692900+yell0wsuit@users.noreply.github.com>
2021-09-22 23:17:55 +0700

    Update website

--- js/saveandexport.js
+++ js/saveandexport.js
@@ -1,23 +1,24 @@
 const saveTemplateAsFile = (filename, dataObjToWrite) => {
-        const blob = new Blob([JSON.stringify(dataObjToWrite)], {
-            type: "text/json"
-        });
-        const link = document.createElement("a");
+    const blob = new Blob([JSON.stringify(dataObjToWrite)], {
+        type: "text/json"
+    });
+    const link = document.createElement("a");
 
-        link.download = filename;
-        link.href = window.URL.createObjectURL(blob);
-        link.dataset.downloadurl = ["text/json", link.download, link.href].join(":");
+    link.download = filename;
+    link.href = window.URL.createObjectURL(blob);
+    link.dataset.downloadurl = ["text/json", link.download, link.href].join(":");
 
-        const evt = new MouseEvent("click", {
-            view: window,
-            bubbles: true,
-            cancelable: true,
-        });
+    const evt = new MouseEvent("click", {
+        view: window,
+        bubbles: true,
+        cancelable: true,
+    });
 
-        link.dispatchEvent(evt);
-        link.remove();
+    link.dispatchEvent(evt);
+    link.remove();
 
 };
+
 const items = {
     ...localStorage
 };
